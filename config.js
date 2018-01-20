c192e761f3ead3d66e9e234ba4cd811b70e25859
Relative <relative@outlook.ie>
2018-01-20 12:33:16 -0500

    Require path in config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-const fs = require('fs')
+const fs = require('fs'),
+  path = require('path')
 const conf = fs.existsSync(path.join(__dirname, 'config.json'))
   ? require(path.join(__dirname, 'config.json'))
   : {};
